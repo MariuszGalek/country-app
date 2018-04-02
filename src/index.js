e4f34407af4811f53be42802b649008928692b40
Mariusz Galek <mgalek1@gmail.com>
2018-04-02 11:51:50 +0200

    Add navigation component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
 import DevTools from './devTools/DevTools';
 import { getCountries } from './actions/actions-countries';
+import Navigation from './presentational/navigation.component';
 
 render(
    	<Provider store={store}>
-   		<div>
-        	<h1>Inicjalizacja projektu</h1>
-        	<DevTools />
-        </div>
+   	    <Router history={hashHistory}>
+   	        <Route path='/' component={Navigation}>
+
+        	</Route>
+        </Router>
     </Provider>,
     document.getElementById('root')
 );
