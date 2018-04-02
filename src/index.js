b903247ac1f65eb546e4afe510472d91991fc9ba
Mariusz Galek <mgalek1@gmail.com>
2018-04-02 13:48:05 +0200

    Add styles file

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
 import routes from './routes';
-import DevTools from './devTools/DevTools';
 
 render(
    	<Provider store={store}>
