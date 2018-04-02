c0741ddfa2a8c7a497481f30dc11eff3f43ac54f
Mariusz Galek <mgalek1@gmail.com>
2018-04-02 12:11:10 +0200

    Add presentational components

--- src/index.js
+++ src/index.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
+import routes from './routes';
 import DevTools from './devTools/DevTools';
-import { getCountries } from './actions/actions-countries';
-import Navigation from './presentational/navigation.component';
 
 render(
    	<Provider store={store}>
-   	    <Router history={hashHistory}>
-   	        <Route path='/' component={Navigation}>
-
-        	</Route>
-        </Router>
+   	    <Router history={hashHistory} routes={routes}/>
     </Provider>,
     document.getElementById('root')
 );
-
-store.dispatch(getCountries());
